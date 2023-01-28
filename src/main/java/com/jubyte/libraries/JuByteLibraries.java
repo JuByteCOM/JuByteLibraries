ec2fc215acdc1b08983d78c5fd4376cde1db2056
JustinSGD <netstat -plant | grep 80>
2023-01-29 00:07:50 +0100

    asdad

---
+++ src/main/java/com/jubyte/libraries/JuByteLibraries.java
@@ -0,0 +1,14 @@
+package com.jubyte.libraries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Justin_SGD
+ * @since 22.09.2022
+ */
+
+public class JuByteLibraries extends JavaPlugin {
+
+    public void onEnable() {
+    }
+}
